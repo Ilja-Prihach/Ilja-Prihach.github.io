ea3c6a323256729a77695676480bf4adb88189ed
Ilja-Prihach <92183327+Ilja-Prihach@users.noreply.github.com>
2023-09-13 20:44:19 +0300

    Add files via upload

---
+++ js/script.js
@@ -0,0 +1,21 @@
+// работа гамбургера
+const hamburger = document.querySelector('.hamburger'),
+      menu = document.querySelector('.menu'),
+      close = document.querySelector('.menu__close');
+
+
+hamburger.addEventListener('click', () => {
+    menu.classList.add('active');
+});
+
+close.addEventListener('click', () => {
+    menu.classList.remove('active');
+});
+
+// автоматический пересчет процентов
+const counters = document.querySelectorAll('.skills__rating-counter'),
+      lines = document.querySelectorAll('.skills__rating-line span');
+
+counters.forEach( (item, i) => {
+    lines[i].style.width = item.innerHTML;
+});
